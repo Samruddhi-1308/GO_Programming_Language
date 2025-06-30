74f863f6eef6c870965b84bfdf30f6c39cd5b303
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 10:58:31 +0530

    Add files via upload

---
+++ GOB014.go
@@ -0,0 +1,11 @@
+//Inserting text Between Outputs
+//If we want any other text in between the outputs we can provide that text inside " " sign.
+
+package main
+import "fmt"
+
+func main() {
+	fmt.Print(3 + 4)
+	fmt.Print(" and ") //Replace the underscore (_) and complete the code
+	fmt.Print(2 + 1)
+}
