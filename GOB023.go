320d00356a5bcf61736b3f9d10a8cdcab2e4deeb
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 11:00:53 +0530

    Add files via upload

---
+++ GOB023.go
@@ -0,0 +1,9 @@
+//Declaring a variable
+
+package main
+import "fmt"
+
+func main() {
+	Number := 19 // Update the blanks with the value assigned to Number
+	fmt.Println(Number) // Output the variable to the console
+}
