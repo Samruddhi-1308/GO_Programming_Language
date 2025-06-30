81590b2785ffd17e3549e21d6485fd6b33bd1ea8
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 22:26:44 +0530

    Add files via upload

---
+++ GOB041.go
@@ -0,0 +1,11 @@
+//String length
+//We can use the len() function to get the length of a string.
+
+
+package main
+import "fmt"
+
+func main() {
+	txt := "NumeroTres"
+	fmt.Print("The length of the word is: ",len(txt))
+}
