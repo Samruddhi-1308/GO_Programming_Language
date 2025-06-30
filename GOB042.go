45ede189b1980323375e58cd5938fc8ed2a789a0
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 22:27:09 +0530

    Add files via upload

---
+++ GOB042.go
@@ -0,0 +1,17 @@
+//Outputting Characters from a String
+
+
+package main
+import "fmt"
+
+func main() {
+	word := "Programming"
+	
+	// Printing characters using their indexes
+    fmt.Println(string(word[2]))    // output : 0
+
+    fmt.Println(string(word[4]))    // output : r
+}
+	
+
+
