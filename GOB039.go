22fdd325247c6b3fe3118fe45fe897e6b717929c
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 11:05:44 +0530

    Add files via upload

---
+++ GOB039.go
@@ -0,0 +1,14 @@
+//Concatenation
+//The '+' sign can be used between strings to add them together to make a new string.
+//This is called concatenation.
+
+
+package main
+import "fmt"
+
+func main() {
+    x := "Hello"
+    y := "World"
+    fmt.Print(x + " " + y)
+	
+}
