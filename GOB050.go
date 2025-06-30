c8bc82cbdb94a9c16921f71f34a71ea0b3cdb07b
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 22:28:29 +0530

    Add files via upload

---
+++ GOB050.go
@@ -0,0 +1,16 @@
+//String Operations
+
+// Update the blanks below to print the first 3 characters of the String
+// There is also a syntax error - try and fix it to debug the code
+
+package main
+import "fmt"
+
+func main() {
+	vari := "String"
+	fmt.Print(string(vari[0]))
+    fmt.Print(string(vari[1]))
+	fmt.Print(string(vari[2]))
+	
+	//p was small in every statement!
+}
