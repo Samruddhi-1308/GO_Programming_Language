7219b27e004d09559f612437221b0e607603dd56
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 11:06:12 +0530

    Add files via upload

---
+++ GOB040.go
@@ -0,0 +1,11 @@
+//Strings and Integers
+//GO uses the + sign for both addition and concatenation.
+
+package main
+import "fmt"
+
+func main() {
+	var num1 string = "25"
+	var num2 string = "69"
+	fmt.Print(num1+num2)
+}
