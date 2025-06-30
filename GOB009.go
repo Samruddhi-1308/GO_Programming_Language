92376c0a67f5dacba56d54249a62229904816a2c
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 10:57:44 +0530

    Add files via upload

---
+++ GOB009.go
@@ -0,0 +1,17 @@
+//Area & Perimeter of Rectangle
+//Write a program for the following problem
+
+//Let’s consider a rectangle of sides 11 and 13.
+//Output the following on separate lines
+//Area of the rectangle
+//Perimeter of the rectangle
+
+
+package main
+import "fmt"
+
+func main() {
+
+	fmt.Println(11 * 13)          // Replace the underscore (_) with the correct digits
+	fmt.Println(2 * (11 + 13))
+}
