9346657cb9efafab9b5220750763c4dde2f4cefa
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 11:04:00 +0530

    Add files via upload

---
+++ GOB033.go
@@ -0,0 +1,14 @@
+// Cost of painting
+
+package main
+
+import "fmt"
+
+func main() {
+	s := 14
+	area := s * s
+	cost := area * 7
+	fmt.Println(area) // area and cost have to be output on separate lines
+	fmt.Print(cost)   // output the $ character after cost without any space
+	fmt.Print("$")
+}
