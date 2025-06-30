8da6beb2cab36a48236dac74715d2f52fb162722
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 22:29:28 +0530

    Add files via upload

---
+++ GOB055.go
@@ -0,0 +1,23 @@
+//Calculator
+
+package main
+import "fmt"
+
+func main() {
+	var a, b int
+    var sum, diff int
+
+    fmt.Scan(&a)
+    fmt.Scan(&b)
+
+    sum = a + b
+    diff = a - b
+
+    fmt.Println("Sum is:", sum) //Used Println to the output on different line
+    fmt.Print("Difference is: ", diff)
+
+
+
+
+
+}
