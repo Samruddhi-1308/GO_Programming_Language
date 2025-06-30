6c64c4fe55ef80c853b587b3dd9133b3c46c7046
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 10:58:06 +0530

    Add files via upload

---
+++ GOB013.go
@@ -0,0 +1,13 @@
+//Inserting space between outputs
+//We can use " " to act as an empty space in between multiple outputs.
+
+
+
+package main
+import "fmt"
+
+func main() {
+	fmt.Print(3 + 4)
+	fmt.Print(" ") // Complete the code by replacing underscore (_)
+ 	fmt.Print(2 + 1)
+}
