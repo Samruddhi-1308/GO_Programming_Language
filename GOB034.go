293ef569081ce85fa7642c640c49b7ec7d7637f7
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 11:04:21 +0530

    Add files via upload

---
+++ GOB034.go
@@ -0,0 +1,23 @@
+// Negative Numbers
+
+package main
+import "fmt"
+
+func main() {
+	a := -50
+	b := 40
+	
+	sum := a + b
+	product := a * b
+	quotient := a/b
+	
+	fmt.Println(sum)
+	fmt.Println(product)
+	fmt.Println(quotient)
+	
+	
+}
+	
+
+	// Complete your code from here
+
