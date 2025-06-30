e9596256140baa91c9ddc1eb77dfbf5561a565c8
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 11:01:16 +0530

    Add files via upload

---
+++ GOB024.go
@@ -0,0 +1,14 @@
+//Addition of Variables
+
+package main
+import "fmt"
+
+func main() {
+    
+    a := 23  // Assign the value 23 to a
+    b := 20  // Assign the value 20 to b
+    
+    fmt.Println(a + b)  // Print the desired output
+
+
+}
