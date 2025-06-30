450cfaf38c09d71783807d4754dd208082bc10bc
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 11:04:50 +0530

    Add files via upload

---
+++ GOB038.go
@@ -0,0 +1,14 @@
+//Creating a String
+//Strings are used for storing text.
+
+
+package main
+import "fmt"
+
+func main() {
+    
+    fruit := "Apple"
+    fmt.Print(fruit)
+//complete your code
+
+}
