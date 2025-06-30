5d8fe22d1f12cc52c38a30cde0758dfbd4795ab9
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-30 22:27:48 +0530

    Add files via upload

---
+++ GOB049.go
@@ -0,0 +1,12 @@
+//Changing Characters in a String
+
+package main
+import "fmt"
+
+func main() {
+	word := "Ocygen"
+	wordBytes := [] rune(word)
+    wordBytes[1] = 'x'
+	word = string(wordBytes)
+	fmt.Println(word)
+}
