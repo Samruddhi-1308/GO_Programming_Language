30ab84ed209821b66d29ff3bb2aee3247aa450cc
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-07-03 11:28:33 +0530

    Add files via upload

---
+++ GOB061.go
@@ -0,0 +1,14 @@
+// Mile to Kilometer Convertor
+
+package main
+import "fmt"
+
+func main() {
+	var mile int
+	var km int
+	fmt.Scan(&mile)
+    km = int(1.60 * float64(mile))
+    fmt.Print(km)
+    fmt.Print(" Kilometers")
+    
+}
